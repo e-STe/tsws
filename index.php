ce135f36e76e3a4985c487da3fbc00545d440349
e-STe <esteban@gressard.net>
2015-12-13 16:01:17 +0100

    Reformatage des fichiers qui ont été écorchés par l'ancien éditeur web --'

--- index.php
+++ index.php
@@ -1,84 +1,76 @@
-<!DOCTYPE html>
-<html lang="fr">
-	<head>
-		<title>The Science WebSite</title>
-		<meta charset="utf-8" />
-        <meta name="keywords" content="sciences, tsws, tsws.ml, the science website, cours, apprendre facile" />
-        <meta name="description" content="Site collaboratif de connaissances scientifiques, apprendre en s'amusant : D" />
-		<meta name="viewport" content="width=device-width, minimum-scale=1.0, maximum-scale=1.0, initial-scale=1" />
-		<link rel="icon" type="image/png" href="images/icones/favicon-32.png" />
-		<link rel="stylesheet" type="text/css" href="css/base.css" />
-		<link rel="stylesheet" type="text/css" media="only screen and (min-width:50px) and (max-width:820px)" href="css/mobile.css" />
-        <script src="scripts/js/jquery.js"></script>
-        <script src="scripts/js/animations.js"></script>
-        <?php 
-        
-        ?>  
-	</head>
-	<body>
-		<div id="body">
-			<div id="logo"> <h1>&nbsp;The Science Website</h1> <a href="http://tsws.ml"></a> </div>
-			<nav id="menu">
-                <img src="images/icones/menu/trait.png" alt="menu" />
-                <ul>
-				    <li> <span><a href="http://tsws.ml">Accueil</a></span> </li>
-    			    <li> <a href="#notnow">Electronique</a> </li>
-    			    <li> <a href="#notnow">Informatique</a> </li>
-    			    <li> <a href="#notnow">Mathématiques</a> </li>
-				    <li> <a href="#notnow">Mécanique</a> </li>
-				    <li> <a href="#notnow">Programmation</a> </li>
-				    <li> <a href="#notnow">Sciences</a> </li>
-                <ul>
-			</nav>
-		
-			<article>
-				<h1>Un nouveau site : tsws !</h1>
-				<p>Bienvenue sur « The Sciences Website », plus connu sous le nom de « TSWS ».
-					Ce site est tout récent, il a été créé pour que vous perceviez les cours différemment et plus simplement,
-					ainsi que pour vous divertir tout en enrichissant vos connaissances. En suivant attentivement et avec
-					motivation les différents cours proposés sur le site, vous observerez votre niveau augmenter et vos
-					difficultés diminuer. Vous pourrez voir et revoir tous les cours autant de fois que désiré. Nous vous
-					souhaitons une agréable visite du site et nous restons à votre disposition pour toutes questions ou conseils,
-					vous pouvez nous joindre par mail ou sur la page de commentaires commune du site.
-					L'équipe TSWS.
-				</p>
+<!DOCTYPE html>
+<html lang="fr">
+	<head>
+		<title>The Science WebSite</title>
+		<meta charset="utf-8" />
+        <meta name="keywords" content="sciences, tsws, tsws.ml, the science website, cours, apprendre facile" />
+        <meta name="description" content="Site collaboratif de connaissances scientifiques, apprendre en s'amusant : D" />
+		<meta name="viewport" content="width=device-width, minimum-scale=1.0, maximum-scale=1.0, initial-scale=1" />
+		<link rel="icon" type="image/png" href="images/icones/favicon-32.png" />
+		<link rel="stylesheet" type="text/css" href="css/base.css" />
+		<link rel="stylesheet" type="text/css" media="only screen and (min-width:50px) and (max-width:820px)" href="css/mobile.css" />
+        <script src="scripts/js/jquery.js"></script>
+        <script src="scripts/js/animations.js"></script>
+        <?php   
+        ?>  
+	</head>
+	<body>
+		<div id="body">
+			<div id="logo"> <h1>&nbsp;The Science Website</h1> <a href="http://tsws.ml"></a> </div>
+			<nav id="menu">
+                <img src="images/icones/menu/trait.png" alt="menu" />
+                <ul>
+				    <li> <span><a href="http://tsws.ml">Accueil</a></span> </li>
+    			    <li> <a href="#notnow">Electronique</a> </li>
+    			    <li> <a href="#notnow">Informatique</a> </li>
+    			    <li> <a href="#notnow">Mathématiques</a> </li>
+				    <li> <a href="#notnow">Mécanique</a> </li>
+				    <li> <a href="#notnow">Programmation</a> </li>
+				    <li> <a href="#notnow">Sciences</a> </li>
+                <ul>
+			</nav>			
+			<article>
+				<h1>Un nouveau site : tsws !</h1>
+				<p>Bienvenue sur « The Sciences Website », plus connu sous le nom de « TSWS ».
+					Ce site est tout récent, il a été créé pour que vous perceviez les cours différemment et plus simplement,
+					ainsi que pour vous divertir tout en enrichissant vos connaissances. En suivant attentivement et avec
+					motivation les différents cours proposés sur le site, vous observerez votre niveau augmenter et vos
+					difficultés diminuer. Vous pourrez voir et revoir tous les cours autant de fois que désiré. Nous vous
+					souhaitons une agréable visite du site et nous restons à votre disposition pour toutes questions ou conseils,
+					vous pouvez nous joindre par mail ou sur la page de commentaires commune du site.
+					L'équipe TSWS.
+				</p>
 			</article>
-			
-				<h1>De nouveaux cours</h1>
-				<p>Cours de mathématiques, de sciences, de programmation bien sûr, 
-                    les cours vont arriver bientôt aussi, faut bien que le site ait une
+				<h1>De nouveaux cours</h1>
+				<p>Cours de mathématiques, de sciences, de programmation bien sûr, 
+                    les cours vont arriver bientôt aussi, faut bien que le site ait une
 					utilité ! Fin décembre les enfants fin décembre ...</p>
-			
-			<section id="promo_div">
-				<div class="promo pr1"><p>HTML5 CSS3</p></div>
-				<div class="promo pr2"><p>Monter un ordinateur</p></div>
-				<div class="promo pr3"><p>Cuisiner une planquette de veau</p></div>
-				<div style="clear: both; width: 100%;"></div>
+			<section id="promo_div">
+				<div class="promo pr1"><p>HTML5 CSS3</p></div>
+				<div class="promo pr2"><p>Monter un ordinateur</p></div>
+				<div class="promo pr3"><p>Cuisiner une planquette de veau</p></div>
+				<div style="clear: both; width: 100%;"></div>
 			</section>
-
-            <section id="comments">
-                <h2>Commentaires</h2>
-                <textarea>Ecrire un commentaire</textarea>
+            <section id="comments">
+                <h2>Commentaires</h2>
+                <textarea>Ecrire un commentaire</textarea>
             </section>
-
-			<footer>
-				<nav id="search"><input type="text" placeholder="Rechercher sur le site ..." /></nav>
-			    <img src="images/icones/logos/logo_electronique.png" alt="logo_electronique" />
-                <img src="images/icones/logos/logo_informatique.png" alt="logo_informatique" />
-				<img src="images/icones/logos/logo_maths.png" alt="logo_mathematiques" />
-                <img src="images/icones/logos/logo_mecanique.png" alt="logo_mecanique" />
-				<img src="images/icones/logos/logo_programmation.png" alt="logo_programmation" />
-				<img src="images/icones/logos/logo_sciences.png" alt="logo_sciences" />
-                <p style="color: #000;"><a>Nouveautés</a> - <a>Contact</a> - <a>A propos</a></p>
-				<p>&copy tsws.ml - <span id="mobile">version mobile</span><span id="desktop">version ordinateur</span></p>
-
+			<footer>
+				<nav id="search"><input type="text" placeholder="Rechercher sur le site ..." /></nav>
+			    <img src="images/icones/logos/logo_electronique.png" alt="logo_electronique" />
+                <img src="images/icones/logos/logo_informatique.png" alt="logo_informatique" />
+				<img src="images/icones/logos/logo_maths.png" alt="logo_mathematiques" />
+                <img src="images/icones/logos/logo_mecanique.png" alt="logo_mecanique" />
+				<img src="images/icones/logos/logo_programmation.png" alt="logo_programmation" />
+				<img src="images/icones/logos/logo_sciences.png" alt="logo_sciences" />
+                <p style="color: #000;"><a>Nouveautés</a> - <a>Contact</a> - <a>A propos</a></p>
+				<p>&copy tsws.ml - <span id="mobile">version mobile</span><span id="desktop">version ordinateur</span></p>
 			</footer>
-			
-			<div id="notnow">
-				<a href="#">X</a>
-				<h1>Oops !</h1>
-				<p>Pas encore de contenu ici ... Désolé, ça arrive !</p>
-			</div>
-		</div>
-	</body>
+			<div id="notnow">
+				<a href="#">X</a>
+				<h1>Oops !</h1>
+				<p>Pas encore de contenu ici ... Désolé, ça arrive !</p>
+			</div>
+		</div>		
+	</body>
 </html>
--- scripts/js/animations.js
+++ scripts/js/animations.js
@@ -1,42 +1,39 @@
-var opened = false;
-var antirafale = false;
-var focused = false;
+var opened = false;
+var antirafale = false;
+var focused = false;
 var already_r = false;
-
-$(document).ready(function() {
-    $('#menu img').click(function() {
-        if(!opened && !antirafale) {
-            antirafale = true;
-            $('#menu img').attr('src', 'images/icones/menu/trait.gif');
-            setTimeout(function(){$('#menu img').attr('src', 'images/icones/menu/croix.png');}, 800);
-            $('#menu li').css('display', 'block');
-            $('#menu').animate({height: '+=450px'}, 1000, "", function(){antirafale = false;});
-            opened = true;
-        } else if(opened && !antirafale) {
-            $('#menu img').attr('src', 'images/icones/menu/croix.gif');
-            setTimeout(function(){$('#menu img').attr('src', 'images/icones/menu/trait.png');}, 800);
-            antirafale = true;
-            $('#menu').animate({height: '-=450px'}, 1000, "", function(){antirafale = false; $('#menu li').css('display', 'none');});
-            opened = false;
-        }
+$(document).ready(function() {
+    $('#menu img').click(function() {
+        if(!opened && !antirafale) {
+            antirafale = true;
+            $('#menu img').attr('src', 'images/icones/menu/trait.gif');
+            setTimeout(function(){$('#menu img').attr('src', 'images/icones/menu/croix.png');}, 800);
+            $('#menu li').css('display', 'block');
+            $('#menu').animate({height: '+=450px'}, 1000, "", function(){antirafale = false;});
+            opened = true;
+        } else if(opened && !antirafale) {
+            $('#menu img').attr('src', 'images/icones/menu/croix.gif');
+            setTimeout(function(){$('#menu img').attr('src', 'images/icones/menu/trait.png');}, 800);
+            antirafale = true;
+            $('#menu').animate({height: '-=450px'}, 1000, "", function(){antirafale = false; $('#menu li').css('display', 'none');});
+            opened = false;
+        }
+    });
+    $('#comments textarea').focus(function() {
+        $('#comments textarea').animate({width: '90%', height: '400px', fontSize: '1.2em'}, 1000);
     });
     
-    $('#comments textarea').focus(function() {
-        $('#comments textarea').animate({width: '90%', height: '400px', fontSize: '1.2em'}, 1000);
+    $('#comments textarea').blur(function() {
+        $('#comments textarea').animate({width: '60%', height: '50px', fontSize: '1em'}, 1000);
     });
     
-    $('#comments textarea').blur(function() {
-        $('#comments textarea').animate({width: '60%', height: '50px', fontSize: '1em'}, 1000);
-    });
-    
-    $(window).resize(function(){
-        var width = $(window).width();
-        if(width >= 820) {
-            $('#menu li').css('display', 'inline-block');
-            $('#menu').css('height', 'auto');
-        } else if(!opened) {
-            $('#menu').css('height', '70px');
-        }
-    });
-    
+    $(window).resize(function(){
+        var width = $(window).width();
+        if(width >= 820) {
+            $('#menu li').css('display', 'inline-block');
+            $('#menu').css('height', 'auto');
+        } else if(!opened) {
+            $('#menu').css('height', '70px');
+        }
+    });
 });
